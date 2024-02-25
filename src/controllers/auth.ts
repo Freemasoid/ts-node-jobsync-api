4e85f57101fe80c860340bcb25ef54b1caed7f91
Freemasoid <freemasoid@icloud.com>
2024-02-25 01:54:31 +0100

    feat: authentication controller converted

---
+++ src/controllers/auth.ts
@@ -0,0 +1,88 @@
+import User from "../models/User.ts";
+import { StatusCodes } from "http-status-codes";
+import {
+  BadRequestError,
+  UnauthenticatedError,
+} from "../errors/custom-errors.ts";
+import { Request, Response } from "express";
+
+async function register(req: Request, res: Response) {
+  const user = await User.create({ ...req.body });
+  const token = user.createJWT();
+  res.status(StatusCodes.CREATED).json({
+    user: {
+      email: user.email,
+      lastName: user.lastName,
+      location: user.location,
+      name: user.name,
+      token,
+    },
+  });
+}
+
+async function login(req: Request, res: Response) {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new BadRequestError("Please provide email and password");
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
+
+  const isPassOk = await user.checkPass(password);
+
+  if (!isPassOk) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
+
+  const token = user.createJWT();
+
+  res.status(StatusCodes.OK).json({
+    user: {
+      email: user.email,
+      lastName: user.lastName,
+      location: user.location,
+      name: user.name,
+      token,
+    },
+  });
+}
+
+async function updateUser(req: Request, res: Response) {
+  const { email, name, lastName, location } = req.body;
+
+  if (!email || !name || !lastName || !location) {
+    throw new BadRequestError("Please provide all values");
+  }
+
+  const user = await User.findOne({ _id: req.user.userID });
+
+  if (!user) {
+    throw new BadRequestError("No user found");
+  }
+
+  user.email = email;
+  user.name = name;
+  user.lastName = lastName;
+  user.location = location;
+
+  await user!.save();
+
+  const token = user.createJWT();
+
+  res.status(StatusCodes.OK).json({
+    user: {
+      email: user.email,
+      lastName: user.lastName,
+      location: user.location,
+      name: user.name,
+      token,
+    },
+  });
+}
+
+export { register, login, updateUser };
