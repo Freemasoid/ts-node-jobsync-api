4bf2cfda0c368f1d9388cfbbaeea8f7f4af5d8b6
Freemasoid <freemasoid@icloud.com>
2024-02-26 19:53:51 +0100

    feat: authentication converted

---
+++ src/middleware/authentication.ts
@@ -0,0 +1,27 @@
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { UnauthenticatedError } from "../errors/custom-errors.ts";
+import { NextFunction, Request, Response } from "express";
+
+async function authMid(req: Request, res: Response, next: NextFunction) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
+    throw new UnauthenticatedError("Authentication invalid");
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const payload = jwt.verify(
+      token,
+      process.env.JWT_SECRET as string,
+    ) as JwtPayload;
+    const testUser = payload.userID === "65650f0721d2576f35e79c23";
+    req.user = { userID: payload.userID, testUser };
+    next();
+  } catch (error) {
+    throw new UnauthenticatedError("Authentication invalid");
+  }
+}
+
+export default authMid;
